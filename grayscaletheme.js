9749c03fb36113528108bf69668660bb9b4db761
rviid <147655674+rviid@users.noreply.github.com>
2023-10-23 09:49:14 +0200

    added local loading, removed cdn

--- grayscaletheme.js
+++ grayscaletheme.js
@@ -1,12 +1,2 @@
-function addCss(filename) {
-  const style = document.createElement('link')
-  style.href = filename
-  style.type = 'text/css'
-  style.rel = 'stylesheet'
-  document.body.append(style)
-}
-
-window.onload = () => {
-  // External CDN for css hosting.
-  addCss("https://rawcdn.githack.com/rviid/league-grayscale-theme/35070af63f5d589e21d622a149989134168b9607/grayscaletheme.css");
-}
+//Imports the thingamabob
+import "./grayscaletheme.css";
